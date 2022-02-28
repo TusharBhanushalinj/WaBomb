3c904106f5c4d7a957ea63080966535d8948e5ec
Tushar <tushar.b@enjayworld.com>
2022-02-28 18:55:35 +0530

    Version:1.0,Iniatial code that bombs in active textfeld and presses enter

---
+++ bomb.py
@@ -0,0 +1,17 @@
+import time
+from typing import Text
+import pyautogui as pg
+
+T = int(input("Delay \n"))
+Text = input("Message \n")
+Time  = int(input("Amount \n"))
+
+time.sleep(5)
+
+i = 0
+
+while (i <= Time-1):
+    time.sleep(T)
+    pg.typewrite(Text)
+    pg.press('enter')
+    i+=1
