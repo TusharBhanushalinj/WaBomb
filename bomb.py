7d131bfdddf5178d97d4e8669ebe7273e8715e30
Tushar <tushar.b@enjayworld.com>
2022-02-28 20:56:50 +0530

    Version:1.0.0

--- bomb.py
+++ bomb.py
@@ -1,12 +1,22 @@
 import time
-from typing import Text
 import pyautogui as pg
+import pyfiglet
 
-T = int(input("Delay \n"))
-Text = input("Message \n")
-Time  = int(input("Amount \n"))
+name = pyfiglet.figlet_format("WaBomb")
+print(name)
 
-time.sleep(15)
+created_by = "\033[1;34m [#] Created By : Tushar Bhanushali \n"
+print(created_by)
+
+war = "\033[1;31m * Warning : This Tool was Just Made For Fun Don't Misuse it if anyone misuses it, it's not creator's reponsiblity * \n"
+print(war)
+
+ver = "\033[1;34m [#] Version : 1.0.0 \n"
+print(ver)
+
+T = int(input("[+] Enter Delay in seconds : "))
+Text = input("[+] Enter Message : ")
+Time  = int(input("[+] Enter Amount : "))
 
 i = 0
 
@@ -15,3 +25,14 @@ while (i <= Time-1):
     pg.typewrite(Text)
     pg.press('enter')
     i += 1
+
+time.sleep(5)
+
+made_by = "\n \033[1;32m [ \u2713 ] Created By : Tushar Bhanushali \n"
+print(made_by)
+
+vers = "\n \033[1;32m [ \u2713 ] Version : 1.0.0 \n"
+print(vers)
+
+success_msg = "\n \033[1;32m [ \u2713 ] Messages Were Sent ! \n"
+print(success_msg)
